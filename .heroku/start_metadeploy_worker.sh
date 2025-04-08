7659ae665cac8128601a9d41a88bd085821d9694
bing <david.bingham@nimbasolutions.com>
2025-04-08 12:16:36 -0600

    fix(worker): Use /tmp instead of /opt for runtime operations

--- .heroku/start_metadeploy_worker.sh
+++ .heroku/start_metadeploy_worker.sh
@@ -1,4 +1,4 @@
 set -e
-mkdir -p /opt/google/chrome
-ln -s /app/.apt/usr/bin/google-chrome /opt/google/chrome/chrome
+mkdir -p /tmp/google/chrome
+ln -s /app/.apt/usr/bin/google-chrome /tmp/google/chrome/chrome
 python manage.py rqworker default
